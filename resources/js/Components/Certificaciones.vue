9cf30d6253a4133bc54fb32a435398f555ac9939
Cristian Figueroa Ferrer <ilandbz@gmail.com>
2025-10-27 11:21:19 -0500

    asd

--- resources/js/Components/Certificaciones.vue
+++ resources/js/Components/Certificaciones.vue
@@ -38,7 +38,7 @@
 
         <!-- ISO 27001 -->
         <div class="flex flex-col items-center gap-4">
-          <img src="/img/iso-27001.webp" alt="ISO 27001" class="w-40 h-40 object-contain" />
+          <img src="/img/iso-27001.jpg" alt="ISO 27001" class="w-40 h-40 object-contain" />
           <a href="/archivos/iso-27001.pdf" target="_blank"
              class="bg-primary text-white px-6 py-2 rounded-xl font-semibold flex items-center gap-2 hover:bg-primary-dark transition">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="22" height="22" class="fill-white"><path d="M448 304h-53.5l-48 48H448c8.822 0 16 7.178 16 16v80c0 8.822-7.178 16-16 16H64c-8.822 0-16-7.178-16-16v-80c0-8.8 7.18-16 16-16h101.5l-48-48H64c-35.35 0-64 28.65-64 64v80c0 35.35 28.65 64 64 64h384c35.35 0 64-28.65 64-64v-80c0-35.3-28.7-64-64-64zM257 368.1l136-136c9.375-9.375 9.375-24.56 0-33.94s-24.56-9.375-33.94 0L280 294.1V24c0-13.25-10.7-24-24-24s-24 10.75-24 24v270.1L136.1 199c-9.375-9.375-24.56-9.375-33.94 0s-9.375 24.56 0 33.94L239 368.1c4.7 5.6 10.8 7.9 17 7.9s12.3-2.3 17-7.9z"/></svg>
