8fe722ee450c8a0f727e990f13ec650ee0fa495f
Cristian Figueroa Ferrer <ilandbz@gmail.com>
2025-12-18 22:05:56 -0500

    asd

--- resources/js/Pages/Institucional/BancoDeDecisiones.vue
+++ resources/js/Pages/Institucional/BancoDeDecisiones.vue
@@ -585,7 +585,6 @@ const range = computed(() => {
                 <th class="px-4 py-3 text-left">CONTRATISTA</th>
                 <th class="px-4 py-3 text-left">ENTIDAD</th>
                 <th class="px-4 py-3 text-left">CONTROVERSIA</th>
-                <th class="px-4 py-3 text-center w-20">LAUDO</th>
               </tr>
             </thead>
             <tbody>
@@ -600,20 +599,6 @@ const range = computed(() => {
                 <td class="px-4 py-3">{{ row.entidad }}</td>
                 <td class="px-4 py-3">
                   {{ row.controversia || '—' }}
-                </td>
-                <td class="px-4 py-3 text-center">
-                  <a
-                    :href="row.pdf"
-                    target="_blank"
-                    rel="noopener"
-                    class="inline-flex items-center justify-center rounded-full p-2 text-primary hover:bg-primary/10"
-                    title="Ver / descargar laudo (PDF)"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-                         width="18" height="18" class="fill-current">
-                      <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H352v64H448v32H64V416H160V352H64z"/>
-                    </svg>
-                  </a>
                 </td>
               </tr>
 
